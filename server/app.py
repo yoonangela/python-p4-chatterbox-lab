f3bcf28cb645e89d7c05a3808b798bdab299f731
Yeji-Yoon <angelayoon0102@gmail.com>
2025-04-06 19:05:08 +0900

    s

--- server/app.py
+++ server/app.py
@@ -14,13 +14,33 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method=='GET':
+        messages = Message.query.order_by(Message.created_at).all()
+        messagelist =[message.to_dict() for message in messages]
+        return make_response(messagelist, 200)
+    elif request.method=='POST':
+        data = request.json
+        newMessage = Message(body=data['body'], username=data['username'])
+        db.session.add(newMessage)
+        db.session.commit()
+        return make_response(newMessage.to_dict(), 201)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id==id).first()
+    # message = db.session.execute(db.select(Message).filter_by(id=id)).scalar_one()
+    if request.method=='PATCH':
+        data = request.json
+        message.body=data['body']
+        db.session.commit()
+        return make_response(message.to_dict())
+    elif request.method=='DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return make_response('deleted', 204)
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
